b0732200aef7a961cddedb5742820f3136c0ad63
“Harshit <hsingla08@gmail.com>
2023-03-02 13:32:58 +0530

    Configure React app for deployment to GitHub Pages

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Scanner from "./Scanner";
+import React from "react";
+
+function App() {
+    return (
+        <div className="App">
+            <Scanner />
+        </div>
+    );
+}
+
+export default App;
