9d0a6ea4ce42755f512f52deda99b269c4666037
“Harshit <hsingla08@gmail.com>
2023-03-02 17:57:55 +0530

    update build for gh-pages

--- src/Scanner.js
+++ src/Scanner.js
@@ -228,23 +228,24 @@ const Scanner = () => {
     // }
   };
 
-  //   useEffect(() => {
-  document.addEventListener(
-    "visibilitychange",
-    function (ev) {
-      console.log(`Tab state1 : ${document.visibilityState}`);
-      if (document.visibilityState === "visible") {
-        getCameraId();
-        setTabChange(true);
-        // stopScan();
-        console.log("cameraId Dikkat h", cameraId);
-      } else if (document.visibilityState === "hidden") {
-        stopScan();
-      }
-    },
-    true
-  );
-  //   }, []);
+  useEffect(() => {
+    document.addEventListener(
+      "visibilitychange",
+      function (ev) {
+        console.log(`Tab state1 : ${document.visibilityState}`);
+        if (document.visibilityState === "visible") {
+          getCameraId();
+          setTabChange(true);
+          // stopScan();
+          console.log("cameraId Dikkat h", cameraId);
+        } else if (document.visibilityState === "hidden") {
+          setTabChange(false);
+          stopScan();
+        }
+      },
+      true
+    );
+  }, []);
 
   //Scanning will be stopped if app loses its focus - have to refactor this code ||Rishav
   //   window.addEventListener(
